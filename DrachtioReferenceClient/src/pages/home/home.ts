acce9ea65b7c50a172c56e451f83eabd0bb03727
Andrew Tan <andrewvmail@gmail.com>
2018-09-20 19:03:51 -0700

    wire it up so it should connect audio

--- DrachtioReferenceClient/src/pages/home/home.ts
+++ DrachtioReferenceClient/src/pages/home/home.ts
@@ -1,48 +1,65 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import SIP from 'sip.js'
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import SIP from "sip.js";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  ua
-  userId
-  toCall
-  session
-  domain = '127.0.0.1'
+  ua;
+  userId;
+  toCall;
+  session;
+  domain = "127.0.0.1";
 
   constructor(public navCtrl: NavController) {
-    console.log(SIP)
-
+    console.log(SIP);
   }
 
-
-  register () {
+  register() {
     this.ua = new SIP.UA({
-      uri: this.userId + '@' + this.domain,
+      uri: this.userId + "@" + this.domain,
       transportOptions: {
-        wsServers: ['ws://' + this.domain]
+        wsServers: ["ws://" + this.domain]
       },
       register: true
     });
 
+    this.ua.on("invite", function(session) {
+      this.trackAdded(session);
+      session.accept();
+    });
   }
 
-  unregister () {
-    this.ua.unregister()
+  unregister() {
+    this.ua.unregister();
   }
-
 
   call() {
-    this.session = this.ua.invite(this.toCall + '@' + this.domain);
+    this.session = this.ua.invite(this.toCall + "@" + this.domain, {
+      sessionDescriptionHandlerOptions: {
+        constraints: {
+          audio: true,
+          video: false
+        }
+      }
+    });
+    this.trackAdded(this.session);
   }
 
+  trackAdded(session) {
+    session.on("trackAdded", function() {
+      var remoteAudio: any = document.getElementById("remoteAudio");
 
+      var pc = session.sessionDescriptionHandler.peerConnection;
+      var remoteStream;
 
+      remoteStream = pc.getRemoteStreams()[0];
 
-
-
+      remoteAudio.srcObject = remoteStream;
+      remoteAudio.volume = 1;
+      remoteAudio.play();
+    });
+  }
 }
