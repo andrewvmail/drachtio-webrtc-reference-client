4054f878e8773a6d0c58daf53abb3c515d95c4b5
Andrew Tan <andrewvmail@gmail.com>
2018-10-13 01:49:38 -0700

    a way to get to privacy policy

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,12 +4,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { connect, ControllerService } from "@cerebral/angular";
 import { RegistrarTabsPage } from '../pages/tabs/tabs'
+import { PrivacyPolicy } from '../pages/privacy-policy/privacy-policy'
+
+declare const window
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = RegistrarTabsPage;
+
+  // push to privacy policy if we have #privacy in the url for store use
+  rootPage:any = window.location.href.indexOf('#privacy') > -1 ? PrivacyPolicy : RegistrarTabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
